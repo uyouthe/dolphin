06df39068d86e26c93578a13caaf7f2d88002bac
Милослав Волосков <melvoloskov@ya.ru>
2017-08-25 10:10:18 +0300

    good redux example build

--- src/base.js
+++ src/base.js
@@ -1,22 +1,19 @@
 'use strict';
 
 // entry point
-// todo errors
 
 require('./css/style.css');
 import {createStore} from 'redux';
 import {model as mock_model} from './fixtures';
 import {draw} from './lib/classes';
-import {moveController} from './lib/controllers';
-import {clone, getRawId} from './lib/tools';
-import {addElement, removeElement, addConnection, removeConnection} from './actions';
+import {clone} from './lib/tools';
+import {reducer} from './reducer';
 
 let diagram = draw.classDiagram();
 let model = clone(mock_model);
-let store = createStore(modelReducer);
+let store = createStore(reducer, model);
 
-build();
-
+rebuild();
 store.subscribe(rebuild);
 
 diagram.on('redraw', diagram.redrawConnections);
@@ -27,87 +24,6 @@ diagram.on('rebuild', function (e) {
 		payload: e.detail.node
 	});
 });
-
-module.exports = {
-	addElement: _addElement,
-	removeElement: _removeElement,
-	addConnection: _addConnection,
-	removeConnection: _removeConnection,
-	getModel
-}
- 
-function _addElement(blueprint) {
-	store.dispatch({
-		type: 'ADD_ELEMENT',
-		payload: blueprint
-	});
-}
-
-function _removeElement(id) {
-	store.dispatch({
-		type: 'REMOVE_ELEMENT',
-		payload: id
-	});
-}
-
-function _addConnection(blueprint) {
-	store.dispatch({
-		type: 'ADD_CONNECTION',
-		payload: blueprint
-	});
-}
-
-function _removeConnection(id) {
-	store.dispatch({
-		type: 'REMOVE_CONNECTION',
-		payload: id
-	});
-}
-
-
-function modelReducer(state = model, action) {
-	switch (action.type) {
-
-		case 'ADD_ELEMENT':
-			return addElement(state, action.payload);
-
-		case 'REMOVE_ELEMENT':
-			return removeElement(state, action.payload);
-
-		case 'ADD_CONNECTION':
-			return addConnection(state, action.payload);
-
-		case 'REMOVE_CONNECTION':
-			return removeConnection(state, action.payload);
-
-		case 'MOVE':
-			return move(state, action.payload);
-
-		default:
-			return state;
-	}
-}
-
-function getModel () {
-	return store.getState();
-}
-
-function move(model, node) {
-	let new_model = clone(model);
-	let node_id = getRawId(node.attr('id'));
-	let new_coords = {
-		x: node.x(),
-		y: node.y()
-	}
-	
-	new_model.elements.forEach(function (elem) {
-		if (elem.id === node_id) {
-			elem.position = new_coords;
-		}
-	});
-
-	return new_model;
-}
 
 function bindControllers(diagram) {
 	diagram.children().forEach(function (child) {
@@ -126,12 +42,8 @@ function bindControllers(diagram) {
 	});
 }
 
-function build() {
-	diagram.fromModel(store.getState());
-	bindControllers(diagram);
-}
-
 function rebuild() {
 	diagram.clear();
-	build();
+	diagram.fromModel(store.getState());
+	bindControllers(diagram);
 }
